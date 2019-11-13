ecc54cd84a8616da33e38a3a03680838ef9c108e
SeungHoon <gksgns2003@gmail.com>
2019-11-13 14:59:00 +0900

    11월 12일
    깃오류로 인해 못올린 파일

---
+++ Project2D_M/Assets/Script/Character/Common/AnimFuntion.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 작성자          : 고은우
+ * 최종 수정 날짜  : 11_13
+ * 팀              : 1팀
+ * 스크립트 용도   : 애니메이터의 자주 쓸것같은 함수 모음
+ */
+public class AnimFuntion : MonoBehaviour
+{
+    private Animator m_animator = null;
+
+    private void Awake()
+    {
+        m_animator = this.GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// _animName의 애니메이션이 진행중인지
+    /// </summary>
+    /// <param name="_animName"></param>
+    /// <returns></returns>
+    public bool IsAnimationPlayingName(string _animName)
+    {
+        return (m_animator.GetCurrentAnimatorStateInfo(0).IsName(_animName) &&
+            m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime <= 0.99f);
+    }
+
+    /// <summary>
+    /// _animTag의 애니메이션이 진행중인지
+    /// </summary>
+    /// <param name="_animTag"></param>
+    /// <returns></returns>
+    public bool IsAnimationPlayingTag(string _animTag)
+    {
+        return (m_animator.GetCurrentAnimatorStateInfo(0).IsTag(_animTag) &&
+            m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.95f);
+    }
+
+    /// <summary>
+    /// 현재 진행중인 애니메이션 클립의 이름
+    /// </summary>
+    /// <returns></returns>
+    public string GetCurrntAnimClipName()
+    {
+        return m_animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+    }
+
+    /// <summary>
+    /// 현재 진행중인 애니메이션의 진행 초
+    /// </summary>
+    /// <returns></returns>
+    public float GetCurrntClipTime()
+    {
+        return m_animator.GetCurrentAnimatorStateInfo(0).length * m_animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+    }
+
+    public bool IsTag(string _animTag)
+    {
+        return m_animator.GetCurrentAnimatorStateInfo(0).IsTag(_animTag);
+    }
+
+    public bool IsName(string _animName)
+    {
+        return GetCurrntAnimClipName() == _animName;
+    }
+
+    public void SetTrigger(string _triggerName)
+    {
+        m_animator.SetTrigger(_triggerName);
+    }
+
+    public void ResetTrigger(string _triggerName)
+    {
+        m_animator.ResetTrigger(_triggerName);
+    }
+
+    public void SetBool(string _boolName, bool _bool)
+    {
+        m_animator.SetBool(_boolName, _bool);
+    }
+
+    public bool GetBool(string _boolName)
+    {
+        return m_animator.GetBool(_boolName);
+    }
+
+    public IEnumerator AnimTiming(float _time)
+    {
+        yield return 0;
+
+        while (true)
+        {
+            if (GetCurrntClipTime() >= _time)
+                break;
+
+            yield return 0;
+        }
+    }
+}
--- Project2D_M/Assets/Script/Character/Player/PlayerNormalAttack.cs
+++ Project2D_M/Assets/Script/Character/Player/PlayerNormalAttack.cs
@@ -5,16 +5,116 @@ using UnityEngine;
 public class PlayerNormalAttack : MonoBehaviour
 {
     private PlayerState m_playerState = null;
+<<<<<<< HEAD
+    private Rigidbody2D m_rigidbody2D = null;
+    private AnimFuntion m_animFuntion = null;
+    private NORMAL_ATTACK_NUM m_eNormalNum;
+    private bool m_normalB;
+
+    public SkeletonAnimation skeletonAnimation;
+
+    private void Awake()
+=======
     private Animator    m_animator = null;
     // Start is called before the first frame update
     void Start()
+>>>>>>> parent of 77029aa... 11.12
     {
         m_playerState   = this.GetComponent<PlayerState>();
+<<<<<<< HEAD
+        m_rigidbody2D   = this.GetComponent<Rigidbody2D>();
+        m_animFuntion   = this.transform.Find("PlayerSpineSprite").GetComponent<AnimFuntion>();
+=======
         m_animator = this.transform.Find("PlayerSpineSprite").GetComponent<Animator>();
+>>>>>>> parent of 77029aa... 11.12
     }
 
     public void NormalAttack()
     {
+<<<<<<< HEAD
+        m_animFuntion.SetTrigger("TriggerNormalAttack");
+
+        if (m_eNormalNum == NORMAL_ATTACK_NUM.NORMAL_NO)
+        {
+            StartCoroutine(AttackCoroutine());
+            m_eNormalNum = NORMAL_ATTACK_NUM.NORMAL_1;
+        }
+    }
+
+    private IEnumerator AttackCoroutine()
+    {
+        string m_sAnimName;
+        m_normalB = false;
+        yield return 0;
+
+        m_rigidbody2D.constraints = RigidbodyConstraints2D.FreezePositionY;
+
+        m_sAnimName = m_animFuntion.GetCurrntAnimClipName();
+        PlayAnimEffect(m_sAnimName);
+
+        while (true)
+        {
+            if (!m_animFuntion.IsName(m_sAnimName))
+            {
+                m_sAnimName = m_animFuntion.GetCurrntAnimClipName();
+                PlayAnimEffect(m_sAnimName);
+            }
+
+            if (!m_animFuntion.IsAnimationPlayingTag("NormalAttack"))
+            {
+                Debug.Log("break");
+                break;
+            }
+
+            PlayAnimSwitch(m_sAnimName);
+
+            yield return 0;
+        }
+
+        m_eNormalNum = NORMAL_ATTACK_NUM.NORMAL_NO;
+        m_sAnimName = "";
+
+        m_rigidbody2D.constraints = RigidbodyConstraints2D.None;
+        m_rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
+        m_rigidbody2D.AddForce(new Vector2(0.1f,0.1f));
+    }
+
+    private void PlayAnimEffect(string _animName)
+    {
+        if (m_animFuntion.IsTag("NormalAttack"))
+        {
+            switch (_animName)
+            {
+                case "attack_3":
+                    skeletonAnimation.AnimationState.SetAnimation(0, "attack_3_1", false);
+                    break;
+                default:
+                    skeletonAnimation.AnimationState.SetAnimation(0, _animName, false);
+                    break;
+            }
+        }
+=======
         m_animator.SetTrigger("TriggerNormalAttack");
+>>>>>>> parent of 77029aa... 11.12
+    }
+
+    private void PlayAnimSwitch(string _animName)
+    {
+        if (m_animFuntion.IsTag("NormalAttack"))
+        {
+            switch (_animName)
+            {
+                case "attack_3":
+                    Debug.Log(m_animFuntion.GetCurrntClipTime());
+                    if (m_animFuntion.GetCurrntClipTime() > 0.95f && !m_normalB)
+                    {
+                        m_normalB = true;
+                        skeletonAnimation.AnimationState.SetAnimation(0, "attack_3_2", false);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
