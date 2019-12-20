3a3df6245bd534d9699adb3eab0bd75e2784ecea
SeungHoon <gksgns2003@gmail.com>
2019-12-20 18:53:36 +0900

    12.20
    고은우 - 소리작업
    박종현 - 캐릭터 스텟 아이템 연동
    퀘스트 유아이 작업
    한승훈 - 몬스터 피사체 만드는중

---
+++ Project2D_M/Assets/Script/Monster/Rootee/MonsterShootAttackCollider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class MonsterShootAttackCollider : ShootAttackCollider
+{
+	private MonsterInfo m_monsterInfo = null;
+	private CinemachineImpulseSource m_cinemachineImpulse = null;
+	private bool m_hited = false;
+
+	protected override void OnTriggerEnter2D(Collider2D collision)
+	{
+		//if(longDamage)
+		if (m_hited)
+			return;
+
+		if (collision.tag == m_sTagName)
+		{
+			ReceiveDamage receiveDamage = collision.gameObject.GetComponent<ReceiveDamage>();
+
+			if (!receiveDamage.bScriptEnable)
+				return;
+
+			if (attackForce != Vector2.zero)
+			{
+				receiveDamage.AddDamageForce(attackForce);
+				m_cinemachineImpulse.GenerateImpulse();
+			}
+
+			if (m_monsterInfo.IsCritical())
+			{
+				m_damage = (int)((m_damage * 1.5f) + 0.5f);
+				receiveDamage.Receive(m_damage, true);
+			}
+			else
+			{
+				receiveDamage.Receive(m_damage, false);
+			}
+
+			if (autoDistroy)
+				ObjectPool.Inst.PushToPool(this.gameObject);
+
+		}
+	}
+
+	public override void SetDamageColliderInfo(int _damage, string _tagName, Vector2 _attackForce, GameObject _order, bool _autoDistroy = true)
+	{
+		m_sTagName = _tagName;
+		orderObject = _order;
+		m_damage = _damage;
+		attackForce = _attackForce;
+		autoDistroy = _autoDistroy;
+
+		m_monsterInfo = m_monsterInfo ?? _order.transform.root.GetComponent<MonsterInfo>();
+		m_cinemachineImpulse = m_cinemachineImpulse ?? this.GetComponent<CinemachineImpulseSource>();
+	}
+
+}
---
+++ Project2D_M/Assets/Script/Monster/Rootee/ProjectileMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rootee : MonoBehaviour
+{
+	private BezierCurve m_bezierCurve = null;
+	private Vector3 m_position;
+	public float speed = 1;
+
+	public void InitProjectile(Vector3 _startPos, float _distance, int _pointCount, float _height,
+		bool _up, bool _left, int _num)
+	{
+		m_bezierCurve = m_bezierCurve ?? new BezierCurve();
+		this.transform.position = _startPos;
+
+		if (!_left)
+			_distance *= -1;
+
+		m_position = new Vector3(_startPos.x + _distance, _startPos.y, _startPos.z);
+		//for(int i = 0)
+
+	}
+
+}
